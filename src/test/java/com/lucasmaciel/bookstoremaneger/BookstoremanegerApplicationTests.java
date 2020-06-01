35fd7bfd1df10b94be67458717c130f9545a933b
Lucas Maciel <lucasmacielgois@gmail.com>
2020-06-01 11:14:58 -0300

    my project

---
+++ src/test/java/com/lucasmaciel/bookstoremaneger/BookstoremanegerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasmaciel.bookstoremaneger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoremanegerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
